2dd194f4f6a1d50d81f2eaed603f049910984fef
Jezreel Buenconsejo <b.jezreel@yahoo.com>
2025-08-06 01:15:17 +0800

    Add umami

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,47 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import AnimatedBackground from "@/components/AnimatedBackground";
 import ClientWrapper from "@/components/ClientWrapper";
+import Script from "next/script";
 
 export const metadata: Metadata = {
-  title: "Jezreel Buenconsejo",
-  description: "Online Portfolio",
+  title: "Jez Buenconsejo | Full Stack Developer",
+  description: "Portfolio of Jez Buenconsejo – Full Stack Developer skilled in TypeScript, React, Go, and more.",
+  openGraph: {
+    title: "Jez Buenconsejo | Full Stack Developer",
+    description: "Explore my portfolio, projects, and tech stack.",
+    url: "https://jezreelbuenconsejo.netlify.app", // or your custom domain
+    type: "website",
+    images: [
+      {
+        url: "https://example.com/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Jezreel Buenconsejo Portfolio Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jez Buenconsejo | Full Stack Developer",
+    description: "Explore my portfolio, projects, and tech stack.",
+    images: ["https://example.com/og-image.png"],
+  },
+  metadataBase: new URL("https://jezreelbuenconsejo.netlify.app"),
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`antialiased lato-regular bg-theme-background relative`}>
+         <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c76ba77f-398f-410b-b3d7-7b770d34c4cf"
+          strategy="beforeInteractive" // Injects in <head> before JS loads
+        />
         <ClientWrapper>
           <AnimatedBackground />
           <Navbar />
